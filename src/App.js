2ca3ace2470bc9fd43078c2b0b3ed6ffe089054e
Pavel Tashev <pta>
2024-01-30 17:23:27 +0200

    Add Settings page.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Favicon from "react-favicon";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Navbar } from './components';
-import { Target, Info, Recycle } from './views';
+import { Target, Info, Recycle, Settings } from './views';
 
 import { StateContextProvider } from './context';
 import faviconImg from './assets/img/favicon.png';
@@ -21,13 +21,14 @@ const App = () => {
                     <div className="column is-1.5 sidebar">
                         <Navbar />
                     </div>
-                    <div className="column area-data">
+                    <div className="column is-5.5 area-data">
                         <Routes>
                             <Route exact path="/" element={<Target />} />
                             <Route exact path="/info" element={<Info />} />
+                            <Route exact path="/settings" element={<Settings />} />
                         </Routes>
                     </div>
-                    <div className="column area-action">
+                    <div className="column is-5 area-action">
                         <Recycle />
                     </div>
                 </div>
--- src/views/Settings.jsx
+++ src/views/Settings.jsx
@@ -1,8 +1,135 @@
 import React from 'react';
+import { useState } from "react";
+
+import { useApi } from '../hooks';
+import { useStateContext } from '../context';
+import { ReactComponent as SvgFeedbackQR } from '../assets/svg/svgFeedbackQR.svg';
 
 const Settings = () => {
+    const [ pin, setPin ] = useState(false);
+    const [ isAuth, setIsAuth ] = useState(false);
+    const [ isAuthError, setIsAuthError ] = useState(false);
+    const [ isAuthPending, setIsAuthPending ] = useState(false);
+
+    const api = useApi();
+    const stateContext = useStateContext();
+
+    function handleAuth() {        
+        if (false === pin) {
+            setIsAuthError(true);
+            return;
+        }
+
+        setIsAuthError(false);
+        setIsAuthPending(true);
+
+        api.auth(pin).then(response => {
+            setPin(false);
+            setIsAuthPending(false);
+
+            if (response.ok) {
+                setIsAuth(true);
+            } else {
+                setIsAuth(false);
+                setIsAuthError(true);
+            }
+        });
+    }
+
+    function handleRestart() {
+        api.reboot().then(response => {
+            if (response.ok) {
+                
+            } else {
+                
+            }
+        });
+    }
+
+    function handleShutDown() {
+        api.shutdown().then(response => {
+            if (response.ok) {
+                
+            } else {
+                
+            }
+        });
+    }
+
+    function handleSignout() {
+        setPin(false);
+        setIsAuth(false);
+        setIsAuthError(false);
+        setIsAuthPending(false);
+    }
+
     return (
-        <div>Settings</div>
+        <div className="content p-5 settings"> 
+            <h1 className="label">Настройки</h1>      
+
+            <div className="pb-6">
+                {
+                    isAuth
+                    ? <>
+                        <div className="columns">
+                            <div className="column is-8 has-text-left">
+                                <button 
+                                    onClick={handleRestart}
+                                    className="button is-danger mr-5" 
+                                >Рестарт</button>
+                                <button 
+                                    onClick={handleShutDown}
+                                    className="button is-danger mr-5" 
+                                >Изключи</button>                            
+                            </div>
+                            <div className="column is-4 has-text-right">
+                                <button 
+                                    onClick={handleSignout}
+                                    className="button is-primary" 
+                                >Излез</button>
+                            </div>    
+                        </div> 
+                    </>
+                    : <>
+                        <div className="field">      
+                            <input                         
+                                type="password" 
+                                className="input" 
+                                placeholder="Въведи PIN" 
+                                value={pin ? pin : ""}
+                                onChange={e => setPin(e.target.value)}
+                            />                        
+                            {isAuthError && (
+                                <p className="has-text-danger">Невалиден PIN. Опитайте отново.</p>
+                            )}
+                        </div>
+                        <div className="field has-text-right">                        
+                            <button 
+                                onClick={handleAuth}
+                                className="button is-primary" 
+                            >Влез {
+                                isAuthPending
+                                ? "..."
+                                : ""
+                            }</button>                        
+                        </div> 
+                    </>
+                }  
+            </div>        
+
+            <div className="mt-6">
+                <h1 className="label">Обратна връзка</h1>
+                <p>Ако възникне грешка с устройство на ReKosh и искате да съобщите за проблема, моля изпратете ни имейл с повече подробности на hi@rekosh.io.</p>
+                {
+                    stateContext.state !== null && stateContext.state.error.status &&
+                    <>
+                        <p className="mt-6">Текущи грешки:</p>
+                        <p className="has-text-danger">{stateContext.state.error.message}</p>
+                    </>
+                }
+                {/* <SvgFeedbackQR /> */}
+            </div>
+        </div>
     );
 }
 
